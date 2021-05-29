62ecec53b45e5c937e1b40291ad9f3152f2f2e5d
Yoshitaka <mznm0824.h.n@gmail.com>
2021-05-30 08:43:14 +0900

    feature: top #1

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,74 @@
 <template>
-  <div class="container">
-    <div>
-      <header class="content-logos">
-        <logo />
-        <span class="plus">+</span>
-        <VuesaxLogo />
-      </header>
-      <h1 class="title">Nuxt.js + Vuesax</h1>
-      <h2 class="subtitle">
-        <a href="https://vuesax.com/">Vuesax</a> is a framework of ui components
-        for <a href="https://vuejs.org/">Vuejs</a>, It was created to make new
-        interfaces that have a new trend and are visually beautiful
-      </h2>
-      <div class="links">
-        <h3 class="h3">Vuesax</h3>
-        <a
-          href="https://vuesax.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://discordapp.com/invite/9dsKtvB"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          Discord
-        </a>
-        <a
-          href="https://github.com/lusaxweb/vuesax"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-      <div class="links">
-        <h3 class="h3">Nuxt.js</h3>
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <vs-row vs-w="12">
+    <vs-col vs-lg="3" vs-md="2" vs-xs="1"></vs-col>
+    <vs-col vs-lg="6" vs-md="8" vs-xs="10">
+      <vs-col vs-type="flex" vs-justify="center">
+        <vs-avatar
+          size="150px"
+          src="https://yoshitaka-portfolio.s3-ap-northeast-1.amazonaws.com/DDF93DD4-80CD-4CA4-84F7-A9E39422DD9F_1_201_a.jpeg"
+        />
+      </vs-col>
+      <vs-col vs-type="flex" vs-justify="center">
+        <h1 class="title">
+          Yoshitaka Portfolio <span class="subtitle">by Nuxt.js</span>
+        </h1>
+      </vs-col>
+      <vs-col>
+        <h2>About</h2>
+        <p class="subtitle">
+          <br />
+          はじめまして、ご覧いただきありがとうございます。<br />
+          バックエンドを中心に業務を行なっている 水沼 能嵩 です。<br /><br />
+          大学卒業後、新卒未経験としてシステム受託開発会社に就職。複数のWebシステム開発経験を経た後、最新技術に対する知見を深め、技術力向上を目指すべく、都内のスタートアップ企業へと転職をしました。<br /><br />
+          現在はCtoCレンタルプラットフォームサービスの新規開発におけるフロントエンドからバックエンド、インフラ構築にあたるまで幅広い開発業務を経験しています。<br /><br />
+          キャッチアップとレスポンスの早さを武器に、常に向上心を持って前向きに業務・自主学習に取り組んでいます。<br /><br />
+          よろしくお願いいたします！
+        </p>
+      </vs-col>
+      <vs-col>
+        <h2>Skill set</h2>
+        <vs-list>
+          <vs-list-header icon="description" title="対応言語"></vs-list-header>
+          <vs-list-item title="PHP / Java / Python / JavaScript"></vs-list-item>
+          <vs-list-header
+            icon="batch_prediction"
+            title="フレームワーク・ライブラリ"
+            color="success"
+          >
+          </vs-list-header>
+          <vs-list-item
+            title=" Laravel / Vue.js / Nuxt.js / jQuery"
+          ></vs-list-item>
+          <vs-list-header icon="storage" title="RDB" color="danger">
+          </vs-list-header>
+          <vs-list-item
+            title=" MySQL / PostgreSQL / SQL Server "
+          ></vs-list-item>
+          <vs-list-header icon="cloud" title="AWS" color="warning">
+          </vs-list-header>
+          <vs-list-item
+            title="EC2 / S3 / RDS / Route53 / APIGateway / Lambda / Elemental MediaConvert / CodeBuild / CodeDeploy / CodePipeline "
+          ></vs-list-item>
+          <vs-list-header icon="build" title="その他" color="dark">
+          </vs-list-header>
+          <vs-list-item title=" GitHub / Docker / SVN"></vs-list-item> </vs-list
+      ></vs-col>
+      <vs-col>
+        <h2>Learning</h2>
+        <vs-chip color="primary">TypeScript</vs-chip>
+        <vs-chip color="primary">Golang</vs-chip>
+        <vs-chip color="success">React</vs-chip>
+        <vs-chip color="success">Next.js</vs-chip>
+        <vs-chip color="warning">ECS</vs-chip>
+        <vs-chip color="dark">Terraform</vs-chip>
+        <vs-chip color="dark">Kurbenetes</vs-chip>
+      </vs-col>
+    </vs-col>
+    <vs-col vs-lg="3" vs-md="2" vs-xs="1"></vs-col>
+  </vs-row>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuesaxLogo from '~/components/VuesaxLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuesaxLogo,
-  },
-}
-</script>
-
+<script></script>
 <style>
 .container {
   margin: 0 auto;
@@ -83,7 +78,6 @@ export default {
   align-items: center;
   text-align: center;
 }
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -95,40 +89,38 @@ export default {
   text-transform: capitalize;
   margin: 25px 0;
 }
-
 .subtitle {
   font-weight: 300;
   font-size: 1.1rem;
   color: #526488;
   word-spacing: 2px;
   padding-bottom: 15px;
-  max-width: 600px;
 }
-
 .subtitle a {
   font-weight: 500;
   color: inherit;
 }
-
 .links {
   padding-top: 15px;
   margin-bottom: 20px;
 }
-
 .content-logos {
   display: flex;
   align-items: center;
   justify-content: center;
   min-width: 500px;
 }
-
 .plus {
   font-size: 2.5rem;
   margin: 15px;
   color: #35495e;
 }
-
-.h3 {
+h2 {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  margin: 20px 0px;
+}
+h3 {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-weight: 400;
